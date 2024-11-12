34ca9ee7f848c37dab8ed096485fb5711eda6693
sol1-boop <112086350+sol1-boop@users.noreply.github.com>
2024-11-13 02:52:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
-TELEGRAM_TOKEN = '6370386978:AAEEeQFUPoeFW1XgJUVNEMVvUJqlH44IQHw'  # Замените на ваш токен
-CHANNEL_ID = '-1002157891114'  # Замените на ваш ID канала или чата
+TELEGRAM_TOKEN = ''  # Замените на ваш токен
+CHANNEL_ID = ''  # Замените на ваш ID канала или чата
 
 users = {
-    'admin': 'mistersol187'
+    'admin': ''
 }
